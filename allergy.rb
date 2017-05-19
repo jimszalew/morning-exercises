ceb9d516f89fa08cec64fef91d7e9376e976cdb7
Jim Szalewski <jim.szalewski@gmail.com>
2017-05-19 07:00:48 -0600

    updated check method

--- allergy.rb
+++ allergy.rb
@@ -4,24 +4,6 @@ class Allergy
     @score = score
   end
 
-  def check
-    index.each_key do |num|
-      if index.include?(@score)
-        index[@score]
-      else
-        math
-      end
-    end
-  end
-
-  # def math
-  #   index.each_key do |num|
-  #     if @score > num
-  #       @score =
-  #     end
-  #   end
-  end
-
   def index
     {1   => 'eggs',
      2   => 'peanuts',
@@ -32,5 +14,22 @@ class Allergy
      64  => 'pollen',
      128 => 'cats'}
   end
+
+
+  def check
+    index.each_key do |num|
+    return index[@score] if num == @score
+    if
+    end
+  end
+
+  # def math
+  #   index.each_key do |num|
+  #     if @score > num
+  #       @score =
+  #     end
+  #   end
+  #end
+
 
 end
--- allergy_test.rb
+++ allergy_test.rb
@@ -1,24 +1,24 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'allergy'
+require './allergy'
 
 class AllergyTest < Minitest::Test
 
   def test_if_allergy_class_exists
-    allergy = Allergy.new
+    allergy = Allergy.new(1)
 
     assert_instance_of Allergy, allergy
   end
 
   def test_it_can_check_for_one_allergy
-    allergy = Allergy.new
+    allergy = Allergy.new(2)
     result = allergy.check
 
     assert_equal "peanuts", result
   end
 
   def test_it_can_check_for_two_allergies
-    allergy = Allergy.new
+    allergy = Allergy.new(3)
     result = allergy.check
 
     assert_equal "peanuts, eggs", result
